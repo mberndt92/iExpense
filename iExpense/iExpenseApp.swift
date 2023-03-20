546de3c3a015b8487cc910e83f54c7be1083c770
mberndt92 <maximilian_berndt@icloud.com>
2023-03-21 08:44:07 +0900

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Maximilian Berndt on 2023/03/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
